8b99c1c2ed2a5a2786651214b4d5faedb817d649
Atacksito <165201064+Atacksito@users.noreply.github.com>
2025-04-29 23:00:47 -0500

    fix: corrección en lógica de entrega

--- PedidosApp/EntregaFactory.cs
+++ PedidosApp/EntregaFactory.cs
@@ -14,7 +14,7 @@ namespace PedidosApp
             if (tipoProducto == "tecnología" && urgente)
                 return new EntregaDron();
             else if (tipoProducto == "accesorio" && peso < 2 && !urgente)
-                //Nuevo tipo de entrega 
+                //Nuevo tipo de entrega
                 return new EntregaBicicleta();
             else if (tipoProducto == "accesorio")
                 return new EntregaMoto();
